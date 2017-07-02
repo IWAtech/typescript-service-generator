d3c0865558d60cc0369ac8f0aeaebe84f9bed8ab
Thomas Klaner <thomas@iwa-tech.com>
2017-07-02 17:05:25 +0200

    integrated prometheus-tracking

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,8 +1,15 @@
+import { Tracker } from 'prometheus-tracking';
 import * as restify from 'restify';
 
 export const server = restify.createServer({
   name: '<%= appname %>',
   version: '0.1.0',
+});
+
+// setup tracking (exports prometheus metrics @ port 9090 /metrics)
+const tracker = Tracker.getInstance(server.name);
+server.on('after', (req: restify.Request, res: restify.Response) => {
+  tracker.trackResponseTime(req.url, req.method, res.statusCode, Tracker.sumUp(req.timers.map((t) => t.time)));
 });
 
 server.use(restify.bodyParser());
